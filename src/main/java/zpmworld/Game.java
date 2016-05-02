2285e02ce950cce6cfc8919fce6ca94381817dcc
Trozer Wincitore <trozer@live.com>
2016-05-02 19:20:27 +0200

    test

--- src/main/java/zpmworld/Game.java
+++ src/main/java/zpmworld/Game.java
@@ -22,7 +22,7 @@ public class Game {
 			Game game = new Game();
 			System.out.println("Üdvözöllek a ZPM világ nevû játékban!");
 			//later check args and run appropiate command
-			System.out.println("janika");
+			System.out.println("janikaa");
 			File argFile = new File("testMap.xml");
 			game.newGame(argFile);
 			game.console();
